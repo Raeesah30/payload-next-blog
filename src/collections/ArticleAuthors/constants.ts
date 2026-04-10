28111de48c7e87e243cba5da5caaaedc23da5e2d
Ra'eesah <raeesahgoolamossen@gmail.com>
2026-04-10 15:38:01 +0400

    feat(seeders): article authors

---
+++ src/collections/ArticleAuthors/constants.ts
@@ -0,0 +1,7 @@
+export const ARTICLE_AUTHOR_ROLE_OPTIONS = {
+    STAFF_WRITER: 'Staff Writer',
+    GUEST_WRITER: 'Guest Writer',
+    FLO_RIDA: 'Flo Rida',
+    CONTRIBUTORS: 'Contributor',
+    EDITOR: 'Editor',
+} as const
--- src/scripts/seed/seeders/admin.seeder.ts
+++ src/scripts/seed/seeders/admin.seeder.ts
@@ -1,11 +1,8 @@
-import { getPayload } from 'payload'
-import config from '@/payload.config'
+import { Payload } from 'payload'
 import { isDuplicateError } from '../lib/is-duplicate-error'
 import { env } from '@/scripts/lib/env'
 
-export async function seedAdmin() {
-    const payload = await getPayload({ config })
-
+export async function seedAdmin(payload: Payload) {
     try {
         const response = await payload.create({
             collection: 'users',
@@ -23,4 +20,3 @@ export async function seedAdmin() {
         }
     }
 }
- 
