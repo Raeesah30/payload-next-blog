3024a3607ed87c884a0d36d933de0b6763633f39
Ra'eesah <raeesahgoolamossen@gmail.com>
2026-04-13 11:07:35 +0400

    chore(deps): next themes

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './globals.css'
+import { ThemeProvider } from 'next-themes'
 
 export const metadata = {
     description: 'A blank template using Payload in a Next.js app.',
@@ -10,7 +11,7 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
     const { children } = props
 
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body>
                 <main>{children}</main>
             </body>
